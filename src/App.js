2c970ca8106f8eeabafd61d8662df74f20ac4e7a
Tom Cao <itom41098@gmail.com>
2019-09-25 18:54:48 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const boardSize = 20;
 function Square(props) {
   return (
     <button
-      className="square"
+      className={"square " + (props.isWinning ? "square--winning" : null)}
       onClick={props.onClick}
     >
       {props.value}
@@ -19,57 +19,18 @@ function Square(props) {
 // -------------BOARD-------------
 class Board extends React.Component
 {
-  constructor(props){
-    super(props);
-    this.state = {
-      squares: Array(boardSize * boardSize).fill(null),
-      xIsNext: true,
-      currentClick: null,
-    }
-  }
-
-  restartGame()
-  {
-    this.setState({
-      squares: Array(boardSize * boardSize).fill(null),
-      xIsNext: true,
-      currentClick: null,
-    });
-  }
-
-  handleClick(i)
-  {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(this.state.currentClick, this.state.squares) || squares[i]) {
-      return;
-    }
-
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-      currentClick: i,
-    });
-  }
-
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        isWinning={this.props.winningSquares.includes(i)}
+        key={"square" + i}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const winner = calculateWinner(this.state.currentClick, this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     const board = [];
 
     for (var i = 0; i < boardSize; i++)
@@ -88,18 +49,11 @@ class Board extends React.Component
 
     return (
       <div>
-      <div>
-        <button
-          className="resetButton"
-          onClick={this.restartGame.bind(this)}
-        >
-        <strong>Restart the game</strong>
-        </button>
-      </div>
-      <div className="board">
-        <div className="status">{status}</div>
-        {board}
-      </div>
+        <div>
+          </div>
+            <div className="board">
+              {board}
+          </div>
       </div>
     );
   }
@@ -108,8 +62,80 @@ class Board extends React.Component
 // -------------GAME-------------
 class Game extends React.Component
 {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(boardSize * boardSize).fill(null),
+        pastCol: null,
+        pastRow: null,
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+      currentClick: null,
+      isDescending: true,
+    }
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+
+    const squares = current.squares.slice();
+    if (calculateWinner(this.state.currentClick, squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+        pastCol: (i % 20) + 1,
+        pastRow: Math.floor(i / 20) + 1,
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+      currentClick: i,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
+  sortHistory() {
+    this.setState({
+      isDescending: !this.state.isDescending
+    });
+  }
+
   render()
   {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(this.state.currentClick, current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to #' + move + "- (col: " + history[move].pastCol + ", row: " + history[move].pastRow + ")" :
+        'Go to game start';
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{move === this.state.stepNumber ? <b>{desc}</b> : desc}</button>
+          </li>
+        )
+    });
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner.player;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="App">
 
@@ -118,15 +144,31 @@ class Game extends React.Component
           <p>
             Welcome to <strong>Tic-Tac-Toe</strong> Vietnamese version!
           </p>
+          <div className="line">
+            <strong>RULE: </strong>
+            Who hits 5 without being block at the 2 ends first is the WINNER!
+            <div className="line"></div>
+          </div>
         </header>
 
         <body className="App-body">
-          <div className="line"></div>
-          <div>
-            <strong>RULE: </strong>
-            Who hits 5 without being block at the 2 ends first is the WINNER!
-          </div>
-          <Board />
+          <div>{status}</div>
+          <table>
+            <td >
+            <Board
+              winningSquares={winner ? winner.line : []}
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
+            </td>
+            <button onClick={() => this.sortHistory()}>
+              Sort by: {this.state.isDescending ? "Descending" : "Ascending"}
+            </button>
+            <td>
+            Scrolling menu:
+              <div className="vertical-menu">{this.state.isDescending ? moves : moves.reverse()}</div>
+            </td>
+          </table>
         </body>
 
       </div>
@@ -170,23 +212,23 @@ function horizontalLine(currentSquare, squares)
   const lines = [
     [squares[currentSquare], squares[currentSquare + 1],
       squares[currentSquare + 2], squares[currentSquare + 3],
-      squares[currentSquare + 4], squares[currentSquare + 5]],
+      squares[currentSquare + 4], squares[currentSquare + 5], squares[currentSquare - 1]],
     [squares[currentSquare], squares[currentSquare - 1],
       squares[currentSquare - 2], squares[currentSquare - 3],
-      squares[currentSquare - 4], squares[currentSquare - 5]]
+      squares[currentSquare - 4], squares[currentSquare - 5], squares[currentSquare + 1]]
   ]
 
   for (let i = 0; i < lines.length; i++)
   {
-    const [a, b, c, d, e, f] = lines[i];
+    const [a, b, c, d, e, f, g] = lines[i];
     if (a
       && a === b
       && a === c
       && a === d
       && a === e
-      && (a === f || f === null))
+      && ((a === f || f === null) || (a === g || g === null)))
       {
-        return a;
+        return {player: a, line: [a, b, c, d, e]};
       }
   }
   return false;
@@ -198,23 +240,23 @@ function verticalLine(currentSquare, squares)
   const lines = [
     [squares[currentSquare], squares[currentSquare + boardSize],
       squares[currentSquare + boardSize * 2], squares[currentSquare + boardSize * 3],
-      squares[currentSquare + boardSize * 4], squares[currentSquare + boardSize * 5]],
+      squares[currentSquare + boardSize * 4], squares[currentSquare + boardSize * 5], squares[currentSquare - boardSize]],
     [squares[currentSquare], squares[currentSquare - boardSize],
       squares[currentSquare - boardSize * 2], squares[currentSquare - boardSize * 3],
-      squares[currentSquare - boardSize * 4], squares[currentSquare - boardSize * 5]]
+      squares[currentSquare - boardSize * 4], squares[currentSquare - boardSize * 5], squares[currentSquare + boardSize]]
   ]
 
   for (let i = 0; i < lines.length; i++)
   {
-    const [a, b, c, d, e, f] = lines[i];
+    const [a, b, c, d, e, f, g] = lines[i];
     if (a
       && a === b
       && a === c
       && a === d
       && a === e
-      && (a === f || f === null))
+      && ((a === f || f === null) || (a === g || g === null)))
       {
-        return a;
+        return {player: a, line: [a, b, c, d, e]};
       }
   }
   return false;
@@ -226,23 +268,23 @@ function slashLine(currentSquare, squares)
   const lines = [
     [squares[currentSquare], squares[currentSquare + boardSize + 1],
       squares[currentSquare + (boardSize * 2) + 2], squares[currentSquare + (boardSize * 3) + 3],
-      squares[currentSquare + (boardSize * 4) + 4], squares[currentSquare + (boardSize * 5) + 5]],
+      squares[currentSquare + (boardSize * 4) + 4], squares[currentSquare + (boardSize * 5) + 5], squares[currentSquare - boardSize - 1]],
     [squares[currentSquare], squares[currentSquare - boardSize - 1],
       squares[currentSquare - (boardSize * 2) - 2], squares[currentSquare - (boardSize * 3) - 3],
-      squares[currentSquare - (boardSize * 4) - 4], squares[currentSquare - (boardSize * 5) - 5]]
+      squares[currentSquare - (boardSize * 4) - 4], squares[currentSquare - (boardSize * 5) - 5], squares[currentSquare + boardSize + 1]]
   ]
 
   for (let i = 0; i < lines.length; i++)
   {
-    const [a, b, c, d, e, f] = lines[i];
+    const [a, b, c, d, e, f, g] = lines[i];
     if (a
       && a === b
       && a === c
       && a === d
       && a === e
-      && (a === f || f === null))
+      && ((a === f || f === null) || (a === g || g === null)))
       {
-        return a;
+        return {player: a, line: [a, b, c, d, e]};
       }
   }
   return false;
@@ -254,23 +296,25 @@ function backSlashLine(currentSquare, squares)
   const lines = [
     [squares[currentSquare], squares[currentSquare + boardSize - 1],
       squares[currentSquare + (boardSize * 2) - 2], squares[currentSquare + (boardSize * 3) - 3],
-      squares[currentSquare + (boardSize * 4) - 4], squares[currentSquare + (boardSize * 5) - 5]],
+      squares[currentSquare + (boardSize * 4) - 4], squares[currentSquare + (boardSize * 5) - 5], squares[currentSquare - boardSize + 1],
+      squares[currentSquare - boardSize + 1]],
     [squares[currentSquare], squares[currentSquare - boardSize + 1],
       squares[currentSquare - (boardSize * 2) + 2], squares[currentSquare - (boardSize * 3) + 3],
-      squares[currentSquare - (boardSize * 4) + 4], squares[currentSquare - (boardSize * 5) + 5]]
+      squares[currentSquare - (boardSize * 4) + 4], squares[currentSquare - (boardSize * 5) + 5], squares[currentSquare + boardSize - 1],
+      squares[currentSquare + boardSize - 1]]
   ]
 
   for (let i = 0; i < lines.length; i++)
   {
-    const [a, b, c, d, e, f] = lines[i];
+    const [a, b, c, d, e, f, g] = lines[i];
     if (a
       && a === b
       && a === c
       && a === d
       && a === e
-      && (a === f || f === null))
+      && ((a === f || f === null) || (a === g || g === null)))
       {
-        return a;
+        return {player: a, line: [a, b, c, d, e]};
       }
   }
   return false;
