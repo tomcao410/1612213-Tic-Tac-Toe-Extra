9ad8e8fe86c4d11b92a9edab2ebfee5e3efe9a90
Tom Cao <itom41098@gmail.com>
2019-10-02 13:20:43 +0700

    Undo code

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,7 +8,7 @@ class Board extends React.Component
 {
   renderSquare(i) {
     return (
-      <Squareas
+      <Square
         isWinning={this.props.winningSquares.includes(i)}
         key={"square" + i}
         value={this.props.squares[i]}
